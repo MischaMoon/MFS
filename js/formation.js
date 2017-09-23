a0693e1bb328dccd16010ec4086fa89924394b48
JeSuisElle <oumayma.dhehibi@esprit.tn>
2017-09-23 09:52:25 -1000

    fr

---
+++ js/formation.js
@@ -0,0 +1,29 @@
+/**
+ * Created by L on 23-Sep-17.
+ */
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+
+});
